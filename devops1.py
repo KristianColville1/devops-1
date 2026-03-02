8cc0a45f7facf95725ab0e375300e28a156ffdc0
KristianColville1 <kristiancolville1@gmail.com>
2026-03-02 21:34:41 +0000

    Added create new instance py code for aws

--- devops1.py
+++ devops1.py
@@ -1,1 +1,10 @@
 
+import boto3
+
+
+ec2 = boto3.resource('ec2')
+new_instances = ec2.create_instances(
+             ImageId='ami-0f3caa1cf4417e51b',
+             MinCount=1,
+             MaxCount=1,
+             InstanceType='t2.nano')
---
+++ steps.md
@@ -0,0 +1,5 @@
+set up aws creds:
+~/.aws/credentials
+
+set ami type
+ami-0f3caa1cf4417e51b
